63c447cdd183516d1b559fccc5435197635aa39a
metur100 <medin_93@live.com>
2020-12-24 19:46:57 +0100

    Added HealthBar and Abilities as ScreenOverlay

---
+++ Assets/Advanture Scripts and Codes/LockRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockRotation : MonoBehaviour
+{
+    //public Canvas nameLable;
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Vector3 namePose = Camera.main.WorldToScreenPoint(this.transform.position);
+    //    nameLable.transform.position = namePose;
+    //}
+
+    public Transform target;
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+        Debug.Log("target is " + screenPos.x + " pixels from the left");
+    }
+}
---
+++ Assets/Advanture Scripts and Codes/PlayerMovementAdvanturerKnight.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementAdvanturerKnight : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public Rigidbody2D rb;
+    public GameObject gameOverUI;
+    public float normalMovementSpeed = 200f;
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * normalMovementSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+            FindObjectOfType<AudioManager>().Play("Jump");
+        }
+        if (Input.GetButtonDown("Jump") && Input.GetButtonDown("meleeAttack"))
+        {
+            jump = true;
+            animator.SetBool("isJumpAttack", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+        if (rb.position.y < -80f)
+        {
+            gameOverUI.SetActive(true);
+        }
+    }
+}
