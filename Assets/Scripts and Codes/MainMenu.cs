429b904394cd631cdb44025e908ab0b877607ce2
metur100 <medin_93@live.com>
2021-01-12 00:05:55 +0100

    Added Advanture button in main menu
    Also added the abilities and controls instructions at the beginning of advanture scene

--- Assets/Scripts and Codes/MainMenu.cs
+++ Assets/Scripts and Codes/MainMenu.cs
@@ -9,6 +9,11 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void PlayAdvanture()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Advanture");
+    }
     public void ExitGame()
     {
         Application.Quit();
