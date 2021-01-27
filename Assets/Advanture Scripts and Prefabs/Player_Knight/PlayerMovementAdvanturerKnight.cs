f4585e477fafdb5797d0496c371c4a99b700370c
metur100 <medin_93@live.com>
2021-01-27 20:33:48 +0100

    Added moving platform on touch and moving platform from x to y
    Also added lights for the caves

--- Assets/Advanture Scripts and Prefabs/Player_Knight/PlayerMovementAdvanturerKnight.cs
+++ Assets/Advanture Scripts and Prefabs/Player_Knight/PlayerMovementAdvanturerKnight.cs
@@ -57,4 +57,15 @@ public class PlayerMovementAdvanturerKnight : MonoBehaviour
             gameOverUIAdvanture.SetActive(true);
         }
     }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Moving_Platform"))
+            this.transform.parent = col.transform;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Moving_Platform"))
+            this.transform.parent = null;
+    }
 }
