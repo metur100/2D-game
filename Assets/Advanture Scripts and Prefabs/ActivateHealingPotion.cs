f1511624d35a369988bfd424677200bdb7ad180c
metur100 <medin_93@live.com>
2021-02-03 21:21:25 +0100

    Adding inventory and active on use healing potions

---
+++ Assets/Advanture Scripts and Prefabs/ActivateHealingPotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateHealingPotion : MonoBehaviour
+{
+    public GameObject healthEffect;
+    public int healing = 20;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player_Knight_Advanturer").transform;
+    }
+
+    public void Use()
+    {
+        Instantiate(healthEffect, player.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
--- Assets/Advanture Scripts and Prefabs/HealingPotion.cs
+++ Assets/Advanture Scripts and Prefabs/HealingPotion.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class HealingPotion : MonoBehaviour
 {
-
     public HealthKnightAdvanturer health;
     [SerializeField]
     private int healing = 20;
