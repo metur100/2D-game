9b2b764af984e8708b534cb40cdbc2f397cdd5fc
metur100 <medin_93@live.com>
2021-02-24 00:31:32 +0100

    Added Cutscene

---
+++ Assets/Advanture Scripts and Prefabs/StartCutscene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCutscene : MonoBehaviour
+{
+    public static bool isCutSceneOn;
+    public Animator camAnim;
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "Player_Knight_Advanturer")
+    //    {
+    //        isCutSceneOn = true;
+    //        camAnim.SetBool("IsCutscene", true);
+    //        Invoke(nameof(StopCutscene), 7f);
+    //    }
+    //}
+    public void StartScene()
+    {
+        isCutSceneOn = true;
+        camAnim.SetBool("IsCutscene", true);
+        Invoke(nameof(StopCutscene), 7f);
+    }
+    void StopCutscene()
+    {
+        isCutSceneOn = false;
+        camAnim.SetBool("IsCutscene", false);
+        Destroy(gameObject);
+    }
+}
