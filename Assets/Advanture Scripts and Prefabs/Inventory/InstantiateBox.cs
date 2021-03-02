38ed83b051a54541e95639375872cbd24b4499cf
metur100 <medin_93@live.com>
2021-03-02 01:18:47 +0100

    Added box item to Trunk_AI

---
+++ Assets/Advanture Scripts and Prefabs/Inventory/InstantiateBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Advanture_Scripts_and_Prefabs.Inventory_Level_2
+{
+    public class InstantiateBox : MonoBehaviour
+    {
+        public GameObject prefabBox;
+        public Transform player;
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player_Knight_Advanturer").transform;
+        }
+        public void Use()
+        {
+            if (player != null)
+            {
+                Instantiate(prefabBox, player.transform.position, Quaternion.identity);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
