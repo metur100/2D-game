01355c91337cce74571f7790c4c9a382a97ef250
metur100 <medin_93@live.com>
2021-03-17 13:20:58 +0100

    Loading bar fully implemented

--- Assets/Advanture Scripts and Prefabs/LevelLoader.cs
+++ Assets/Advanture Scripts and Prefabs/LevelLoader.cs
@@ -7,9 +7,8 @@ public class LevelLoader : MonoBehaviour
 {
     public GameObject loadingScreenObj;
     public Slider slider;
-
+    public GameObject [] screens;
     AsyncOperation async;
-
     public void LoadScreenExample(int lvl)
     {
         StartCoroutine(LoadingScreen(lvl));
@@ -18,6 +17,10 @@ public class LevelLoader : MonoBehaviour
     IEnumerator LoadingScreen(int level)
     {
         loadingScreenObj.SetActive(true);
+        for(int i = 0; i < screens.Length; i++)
+        {
+            screens[i].SetActive(false);
+        }
         async = SceneManager.LoadSceneAsync(level);
         async.allowSceneActivation = false;
 
